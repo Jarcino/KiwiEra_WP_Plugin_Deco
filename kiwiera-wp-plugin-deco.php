edd91b471645b62b8c27be5f2125c4ee2a3a272f
Jarcino <jvcorstorphine@gmail.com>
2024-08-20 13:52:31 +1200

    feat: created index.php and main file for plugin dev

---
+++ kiwiera-wp-plugin-deco.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Website Decorations
+ * Description: A plugin that allows users to add decorations to their e-commerce site.
+ * Author: Jamie Corstorphine
+ * Version: 1.0.0
+ * Text Domain: website-decorations
+ * 
+ */
+
